2d0f5a3b1f5cda9aad7f8209fb9222df8897f4fb
Vyshnavi <vyshnavip2912@gmail.com>
2026-01-25 12:01:31 +0530

    Added departments table

---
+++ 04_departments.sql
@@ -0,0 +1,11 @@
+CREATE TABLE departments (
+    department_id INT,
+    department_name VARCHAR(50),
+    manager_name VARCHAR(50)
+);
+
+INSERT INTO departments VALUES
+(1, 'IT', 'Rakesh'),
+(2, 'HR', 'Sonia'),
+(3, 'Finance', 'Manoj'),
+(4, 'Sales', 'Neeraj');
