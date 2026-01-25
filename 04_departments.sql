38631128a3c3e2431ead799c8f3f6641c169e67e
Vyshnavi <vyshnavip2912@gmail.com>
2026-01-25 13:32:32 +0530

    Update 04_departments.sql

--- 04_departments.sql
+++ 04_departments.sql
@@ -5,7 +5,7 @@ CREATE TABLE departments (
 );
 
 INSERT INTO departments VALUES
-(1, 'IT', 'Rakesh'),
-(2, 'HR', 'Sonia'),
+(1, 'IT', 'Vyshnavi'),
+(2, 'HR', 'Ram'),
 (3, 'Finance', 'Manoj'),
 (4, 'Sales', 'Neeraj');
