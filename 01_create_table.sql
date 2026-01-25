409ca07b1420a5da1f4f31c653a33671befa607b
Vyshnavi <vyshnavip2912@gmail.com>
2026-01-25 11:52:22 +0530

    Created employees table schema

---
+++ 01_create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    employee_id INT,
+    name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    department VARCHAR(30),
+    city VARCHAR(30),
+    salary INT,
+    joining_date DATE,
+    performance_rating INT
+);
